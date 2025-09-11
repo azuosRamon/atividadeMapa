78d67168d499396c1299cefadef10196f50e3231
erika-bs <167558855+erika-bs@users.noreply.github.com>
2025-09-11 20:40:59 -0300

    Update MenuCursos.js

--- src/components/MenuCursos.js
+++ src/components/MenuCursos.js
@@ -12,7 +12,7 @@ import DivSeparador from "./SubDivSeparador";
 import TabelaCompleta from "./SubTabela";
 import Colapse from "./SubColapse";
 import cores from "./Cores";
-import useBancoDeDados from "./UseBancoDeDados"; 
+import useBancoDeDados from "./UseBancoDados"; 
 
 const FormGrid = styled.form`
   gap: 10px;
