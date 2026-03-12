c2299a749e863d09b7fd9d0d5f9c1a3479fcd9b6
azuosRamon <167565029+azuosRamon@users.noreply.github.com>
2026-03-12 18:02:01 -0300

    flask-cors

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,10 @@ from redis import Redis
 from dotenv import load_dotenv
 import os
 import json
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
 # 🔹 Carrega variáveis de ambiente
 load_dotenv()
